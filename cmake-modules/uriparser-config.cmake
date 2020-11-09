79c65466537358fd1552c6364afdcf7120993744
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2020-11-10 04:49:27 +0500

    Install uriparser library

--- cmake-modules/uriparser-config.cmake
+++ cmake-modules/uriparser-config.cmake
@@ -4,8 +4,7 @@ ExternalProject_Add(URIParser
     URL https://github.com/uriparser/uriparser/releases/download/uriparser-0.9.4/uriparser-0.9.4.tar.gz
     CMAKE_ARGS 
         -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-        -DCMAKE_BUILD_TYPE:STRING=Release
+        -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
         -DURIPARSER_BUILD_TESTS:BOOL=OFF
         -DURIPARSER_BUILD_DOCS:BOOL=OFF
-        -DBUILD_SHARED_LIBS:BOOL=OFF
 )
