7c8d8786049dff5df16d6f2ad57291d4bde2e68a
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2020-11-10 05:23:13 +0500

    Added parsing for scheme, host port, path

--- include/uriparserpp/uriparserpp.hpp
+++ include/uriparserpp/uriparserpp.hpp
@@ -1,6 +1,8 @@
 #ifndef URI_PARSER_PP_H
 #define URI_PARSER_PP_H
 
+#include <list>
+#include <stdexcept>
 #include <string>
 #include <uriparser/Uri.h>
 
@@ -20,6 +22,51 @@ class uri
    }
    uri(const uri &) = delete;             // non construction-copyable
    uri &operator=(const uri &) = delete;  // non copyable
+
+   std::string scheme()
+   {
+      if (_url.scheme.first != NULL && _url.scheme.afterLast != NULL)
+      {
+         return std::string(_url.scheme.first, _url.scheme.afterLast);
+      }
+      return "";
+   }
+
+   std::string host()
+   {
+      if (_url.hostText.first != NULL && _url.hostText.afterLast != NULL)
+      {
+         return std::string(_url.hostText.first, _url.hostText.afterLast);
+      }
+      return "";
+   }
+
+   std::string port()
+   {
+      if (_url.portText.first != NULL && _url.portText.afterLast != NULL)
+      {
+         return std::string(_url.portText.first, _url.portText.afterLast);
+      }
+      return "";
+   }
+
+   std::string path()
+   {
+      std::string            path = "";
+      std::list<std::string> pathSegments;
+
+      for (auto segment = _url.pathHead; segment != NULL; segment = segment->next)
+      {
+         if (segment->text.first != NULL && segment->text.afterLast != NULL)
+         {
+            pathSegments.push_back(std::string(segment->text.first, segment->text.afterLast));
+         }
+      }
+      std::for_each(pathSegments.begin(), pathSegments.end(), [&path](const auto &segment) { path += "/" + segment; });
+
+      return path;
+   }
+
    ~uri()
    {
       uriFreeUriMembersA(&_url);
--- tests/uriparserpp_test.cpp
+++ tests/uriparserpp_test.cpp
@@ -6,10 +6,14 @@ using namespace uriparserpp;
 
 TEST_CASE("Parsing")
 {
-   uri uri("http://www.google.com/");
+   uri uri("http://user:pass@www.test.com:81/static/index.html?q=books#fragment");
 
-   // REQUIRE(uri.scheme() == "http");
-   // REQUIRE(uri.host() == "www.google.com");
-   // REQUIRE(uri.path() == "/");
-   REQUIRE(1 == 1);
+   REQUIRE(uri.scheme() == "http");
+   REQUIRE(uri.host() == "www.test.com");
+   REQUIRE(uri.port() == "81");
+   REQUIRE(uri.path() == "/static/index.html");
+
+   // uri.userInfo()   // user:pass
+   //    uri.query()   // q=books
+   //    uri.anchor()  // fragment
 }
